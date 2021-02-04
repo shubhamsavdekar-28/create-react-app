c38706d9dd4631fa6410ca40c8cb89abb8843f69
shubhamsavdekar-28 <shubham@bookmyticket.com>
2021-02-04 10:47:28 +0530

    done

---
+++ db.js
@@ -0,0 +1,10 @@
+//DB connection
+const config = {
+  user: "Sqladmin",
+  password: "Venus@007",
+  server: "180.179.227.220",
+  database: "Spectramed",
+  port: 21443,
+};
+
+module.exports = config;
